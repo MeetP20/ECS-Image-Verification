e584a49982a5f76178e70794b206633182316f94
MeetP20 <124292920+MeetP20@users.noreply.github.com>
2026-09-09 18:40:25 +0530

    Add variables for specific-clusters example

---
+++ examples/specific-clusters/variables.tf
@@ -0,0 +1,29 @@
+variable "lambda_name" {
+  type        = string
+  description = "Lambda function name."
+}
+
+variable "lambda_image_uri" {
+  type        = string
+  description = "Existing ECR URI of the Lambda container image."
+}
+
+variable "sns_topic_name" {
+  type        = string
+  description = "SNS topic name."
+}
+
+variable "email" {
+  type        = string
+  description = "Email address for SNS alerts."
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region."
+}
+
+variable "cluster_arns" {
+  type        = list(string)
+  description = "ECS cluster ARNs to monitor."
+}
